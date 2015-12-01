db93c278c62ad78569b540a79c93563b484cbb64
Terry <lict@novasoftware.cn>
2015-12-02 00:01:16 +0800

    ----------

--- scripts/init.js
+++ scripts/init.js
@@ -2,7 +2,7 @@
 //ons.disableAutoStatusBarFill();  // (Monaca enables StatusBar plugin by  
 var MSG_RETUIREDNETWORK = {title:'Internet Connection',content:'Sorry, a network connection is required, please try later.'};
 var MSG_LOGINFAILED = {title:'Incorrect Password',content:'Please check password and try again.'};
-var MSG_SYSTEMERROR = {title:'System Error',content:'There has been an error,Please contact a member of the Aga Khan Health Board. \r\n Error Code:{0}'};
+var MSG_SYSTEMERROR = {title:'System Error',content:'There has been an error,Please contact <a href="mailto:enquiries@iiuk.org">enquiries@iiuk.org</a>. \r\n Error Code:{0}'};
 
 var pushNotification;
 var module = ons.bootstrap('AKHB', ['onsen','ngTouch']);
