308a569beadf750f410662c7972e586d3e86fef9
Terry <lict@novasoftware.cn>
2016-02-25 12:07:44 +0800

    ------------

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,7 @@
 //ons.disableAutoStatusBarFill();  // (Monaca enables StatusBar plugin by  
 var MSG_RETUIREDNETWORK = {title:'Internet Connection',content:'Sorry, a network connection is required, please try later.'};
 var MSG_LOGINFAILED = {title:'Incorrect Password',content:'Please check password and try again.'};
-var MSG_SYSTEMERROR = {title:'System Error',content:'There has been an error, Please contact <a href="mailto:enquiries@iiuk.org">enquiries@iiuk.org</a>. <br /> Error Code:{0}'};
+var MSG_SYSTEMERROR = {title:'System Error',content:'Please check you have a network connection. If issues persist, please contact <a href="mailto:enquiries@iiuk.org">enquiries@iiuk.org</a>. <br /> Error Code:100'};
 
 var pushNotification;
 var module = ons.bootstrap('AKHB', ['onsen','ngTouch']);
@@ -957,7 +957,7 @@ function sendRegistionId(id){
 
 // notificationFeedback Service
 function notificationFeedback(buttonIndex,passedData) {
-	var url = window.AKHB.config.remoteAddress+'?type=5&uuid='+AKHB.user.id+'other='+passedData+'buttonIndex='+buttonIndex;
+	var url = window.AKHB.config.remoteAddress+'?type=5&uuid='+AKHB.user.id+'&other='+passedData+'&buttonIndex='+buttonIndex;
 	$.get(url,function(data){
 	})
 }
